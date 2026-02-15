0ce2c2e1abf0a428261b7d0668d54b567ec1d1f1
Yevhen Vasyliev <ye.vasyliev@gmail.com>
2026-02-15 11:34:40 +0200

    feat: create `InfosService`

--- src/main/java/io/github/yvasyliev/deezer/service/GenreService.java
+++ src/main/java/io/github/yvasyliev/deezer/service/GenreService.java
@@ -21,14 +21,29 @@ public interface GenreService {
     @RequestLine("GET /genre/{genreId}/artists")
     CompletableFuture<Page<Artist>> getArtists(@Param("genreId") int genreId);
 
+    /**
+     * Returns a genre.
+     *
+     * @param genreId the genre ID
+     * @return a genre
+     */
     @RequestLine("GET /genre/{genreId}")
     CompletableFuture<Genre> getGenre(@Param("genreId") int genreId);
 
+    /**
+     * Returns all genres.
+     *
+     * @return all genres
+     */
     @RequestLine("GET /genre")
     CompletableFuture<Page<Genre>> getGenres();
 
-    //TODO: get podcasts
-
+    /**
+     * Returns all radios for a genre.
+     *
+     * @param genreId the genre ID
+     * @return all radios for a genre
+     */
     @RequestLine("GET /genre/{genreId}/radios")
     CompletableFuture<Page<Radio>> getRadios(@Param("genreId") int genreId);
 }
---
+++ src/main/java/io/github/yvasyliev/deezer/service/InfosService.java
@@ -0,0 +1,20 @@
+package io.github.yvasyliev.deezer.service;
+
+import feign.Headers;
+import feign.Param;
+import feign.RequestLine;
+import io.github.yvasyliev.deezer.model.Infos;
+
+import java.util.concurrent.CompletableFuture;
+
+@Headers("Accept: application/json")
+public interface InfosService {
+    /**
+     * Gets the information about the API in the current country.
+     *
+     * @param accessToken Deezer access token
+     * @return the information about the API in the current country
+     */
+    @RequestLine("GET /infos?access_token={accessToken}")
+    CompletableFuture<Infos> getInfos(@Param("accessToken") String accessToken);
+}
