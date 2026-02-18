95b4e07baf8644a607f9a1db41a838c1fdbf768f
Yevhen Vasyliev <ye.vasyliev@gmail.com>
2026-02-18 16:46:02 +0200

    feat: create `PodcastService`

---
+++ src/main/java/io/github/yvasyliev/deezer/service/PodcastService.java
@@ -0,0 +1,36 @@
+package io.github.yvasyliev.deezer.service;
+
+import feign.Headers;
+import feign.Param;
+import feign.RequestLine;
+import io.github.yvasyliev.deezer.model.Episode;
+import io.github.yvasyliev.deezer.model.Page;
+import io.github.yvasyliev.deezer.model.Podcast;
+
+import java.util.concurrent.CompletableFuture;
+
+@Headers("Accept: application/json")
+public interface PodcastService {
+    /**
+     * Returns a list of episodes for a podcast.
+     *
+     * @param podcastId   the podcast ID
+     * @param accessToken Deezer access token
+     * @return a list of episodes for a podcast
+     */
+    @RequestLine("GET /podcast/{podcastId}/episodes?access_token={accessToken}")
+    CompletableFuture<Page<Episode>> getEpisodes(
+            @Param("podcastId") long podcastId,
+            @Param("accessToken") String accessToken
+    );
+
+    /**
+     * Returns a podcast.
+     *
+     * @param podcastId   the podcast ID
+     * @param accessToken Deezer access token
+     * @return a podcast
+     */
+    @RequestLine("GET /podcast/{podcastId}?access_token={accessToken}")
+    CompletableFuture<Podcast> getPodcast(@Param("podcastId") long podcastId, @Param("accessToken") String accessToken);
+}
