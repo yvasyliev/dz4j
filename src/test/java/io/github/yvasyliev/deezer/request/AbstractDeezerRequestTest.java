81072ad39e347b74703e89534dd6058b3ace69fd
Yevhen Vasyliev <ye.vasyliev@gmail.com>
2026-03-11 11:36:39 +0200

    feat: add tests for AbstractDeezerRequest and CreatePlaylistDeezerRequest classes

---
+++ src/test/java/io/github/yvasyliev/deezer/request/AbstractDeezerRequestTest.java
@@ -0,0 +1,104 @@
+package io.github.yvasyliev.deezer.request;
+
+import io.github.yvasyliev.deezer.exception.DeezerException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+import java.util.function.Supplier;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class AbstractDeezerRequestTest {
+    @Mock private Supplier<CompletableFuture<String>> asyncMethod;
+    private DeezerRequest<String> request;
+
+    @BeforeEach
+    void setUp() {
+        request = new AbstractDeezerRequest<>() {
+            @Override
+            protected CompletableFuture<String> doExecuteAsync() {
+                return asyncMethod.get();
+            }
+        };
+    }
+
+    @Test
+    void shouldExecuteSuccessfully() {
+        var expected = "result";
+
+        when(asyncMethod.get()).thenReturn(CompletableFuture.completedFuture(expected));
+
+        var actual = request.execute();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldExecuteAsyncSuccessfully() {
+        var expected = "result";
+
+        when(asyncMethod.get()).thenReturn(CompletableFuture.completedFuture(expected));
+
+        var actual = request.executeAsync().join();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldRethrowDeezerException() {
+        when(asyncMethod.get()).thenReturn(CompletableFuture.failedFuture(mock(DeezerException.class)));
+
+        assertThrows(DeezerException.class, request::execute);
+    }
+
+    @Test
+    void shouldRethrowDeezerExceptionAsync() {
+        when(asyncMethod.get()).thenReturn(CompletableFuture.failedFuture(mock(DeezerException.class)));
+
+        var e = assertThrows(CompletionException.class, () -> request.executeAsync().join());
+
+        assertThat(e).hasCauseInstanceOf(DeezerException.class);
+    }
+
+    @Test
+    void shouldRethrowDeezerExceptionCause() {
+        when(asyncMethod.get()).thenReturn(CompletableFuture.failedFuture(new Exception(new DeezerException("boom"))));
+
+        assertThrows(DeezerException.class, request::execute);
+    }
+
+    @Test
+    void shouldRethrowDeezerExceptionCauseAsync() {
+        when(asyncMethod.get()).thenReturn(CompletableFuture.failedFuture(new Exception(new DeezerException("boom"))));
+
+        var e = assertThrows(CompletionException.class, () -> request.executeAsync().join());
+
+        assertThat(e).hasCauseInstanceOf(DeezerException.class);
+    }
+
+    @Test
+    void shouldWrapOtherExceptions() {
+        when(asyncMethod.get()).thenReturn(CompletableFuture.failedFuture(new Exception("boom")));
+
+        assertThrows(DeezerException.class, request::execute);
+    }
+
+    @Test
+    void shouldWrapOtherExceptionsAsync() {
+        when(asyncMethod.get()).thenReturn(CompletableFuture.failedFuture(new Exception("boom")));
+
+        var e = assertThrows(CompletionException.class, () -> request.executeAsync().join());
+
+        assertThat(e).hasCauseInstanceOf(DeezerException.class);
+    }
+}
