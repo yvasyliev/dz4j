21afcbc74990ae8f770116c9c8b8cc85d2a06cad
Yevhen Vasyliev <ye.vasyliev@gmail.com>
2026-03-16 10:42:52 +0200

    feat: add WireMock integration tests for ArtistIT to validate artist details, albums, fans, playlists, radio, related artists, and top tracks

--- src/integrationTest/java/io/github/yvasyliev/deezer/ArtistIT.java
+++ src/integrationTest/java/io/github/yvasyliev/deezer/ArtistIT.java
@@ -1,140 +1,257 @@
 package io.github.yvasyliev.deezer;
 
+import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import io.github.yvasyliev.deezer.model.Album;
 import io.github.yvasyliev.deezer.model.Artist;
 import io.github.yvasyliev.deezer.model.Page;
 import io.github.yvasyliev.deezer.model.Playlist;
 import io.github.yvasyliev.deezer.model.Track;
 import io.github.yvasyliev.deezer.model.User;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.FieldSource;
+import io.github.yvasyliev.deezer.request.DeezerRequest;
+import io.github.yvasyliev.deezer.util.DeezerDefaults;
+import lombok.Cleanup;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import tools.jackson.core.type.TypeReference;
+import tools.jackson.databind.json.JsonMapper;
 
 import java.io.IOException;
-import java.util.function.Supplier;
-import java.util.stream.Stream;
+import java.util.Objects;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.okJson;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathTemplate;
-import static org.junit.jupiter.params.provider.Arguments.arguments;
 
-class ArtistIT extends AbstractDeezerClientIT {
-    private static final Supplier<Stream<Arguments>> testSuccessfulScenario = () -> {
+@WireMockTest
+class ArtistIT {
+    private static final JsonMapper MAPPER = DeezerDefaults.jsonMapper();
+    private DeezerClient deezerClient;
+
+    @BeforeEach
+    void setUp(WireMockRuntimeInfo wmRuntimeInfo) {
+        deezerClient = DeezerClient.builder()
+                .apiBaseUrl(wmRuntimeInfo.getHttpBaseUrl())
+                .build();
+    }
+
+    @Test
+    void shouldReturnArtist() throws IOException {
+        var artistId = 5587890L;
+        var body = read("/response/artist/get-artist.json");
+        var expected = MAPPER.readValue(body, Artist.class);
+
+        stubFor(get(urlPathTemplate("/artist/{artistId}"))
+                .withPathParam("artistId", equalTo(String.valueOf(artistId)))
+                .willReturn(okJson(body))
+        );
+
+        assertEquals(expected, deezerClient.artist().getArtist(artistId));
+    }
+
+    @Test
+    void shouldReturnAlbums() throws IOException {
+        var artistId = 5587890L;
+        var body = read("/response/artist/get-albums.json");
+        var expected = MAPPER.readValue(body, new TypeReference<Page<Album>>() {});
+
+        stubFor(get(urlPathTemplate("/artist/{artistId}/albums"))
+                .withPathParam("artistId", equalTo(String.valueOf(artistId)))
+                .willReturn(okJson(body))
+        );
+
+        assertEquals(expected, deezerClient.artist().getAlbums(artistId));
+    }
+
+    @Test
+    void shouldReturnAlbumsWithPagination() throws IOException {
         var artistId = 5587890L;
         var index = 5;
         var limit = 10;
+        var body = read("/response/artist/get-albums.json");
+        var expected = MAPPER.readValue(body, new TypeReference<Page<Album>>() {});
 
-        return Stream.of(
-                arguments("artist", new StubArguments<Artist>()
-                        .mappingBuilder(get(urlPathTemplate("/artist/{artistId}")))
-                        .pathParam("artistId", artistId)
-                        .file("/response/artist/get-artist.json")
-                        .methodFactory(client -> client.artist().getArtist(artistId))
-                        .type(new TypeReference<>() {})
-                ),
-                arguments("albums", new StubArguments<Page<Album>>()
-                        .mappingBuilder(get(urlPathTemplate("/artist/{artistId}/albums")))
-                        .pathParam("artistId", artistId)
-                        .file("/response/artist/get-albums.json")
-                        .methodFactory(client -> client.artist().getAlbums(artistId))
-                        .type(new TypeReference<>() {})
-                ),
-                arguments("albums with pagination", new StubArguments<Page<Album>>()
-                        .mappingBuilder(get(urlPathTemplate("/artist/{artistId}/albums")))
-                        .pathParam("artistId", artistId)
-                        .queryParam("index", index)
-                        .queryParam("limit", limit)
-                        .file("/response/artist/get-albums.json")
-                        .methodFactory(client -> client.artist().getAlbums(artistId).index(index).limit(limit))
-                        .type(new TypeReference<>() {})
-                ),
-                arguments("fans", new StubArguments<Page<User>>()
-                        .mappingBuilder(get(urlPathTemplate("/artist/{artistId}/fans")))
-                        .pathParam("artistId", artistId)
-                        .file("/response/artist/get-fans.json")
-                        .methodFactory(client -> client.artist().getFans(artistId))
-                        .type(new TypeReference<>() {})
-                ),
-                arguments("fans with pagination", new StubArguments<Page<User>>()
-                        .mappingBuilder(get(urlPathTemplate("/artist/{artistId}/fans")))
-                        .pathParam("artistId", artistId)
-                        .queryParam("index", index)
-                        .queryParam("limit", limit)
-                        .file("/response/artist/get-fans.json")
-                        .methodFactory(client -> client.artist().getFans(artistId).index(index).limit(limit))
-                        .type(new TypeReference<>() {})
-                ),
-                arguments("playlists", new StubArguments<Page<Playlist>>()
-                        .mappingBuilder(get(urlPathTemplate("/artist/{artistId}/playlists")))
-                        .pathParam("artistId", artistId)
-                        .file("/response/artist/get-playlists.json")
-                        .methodFactory(client -> client.artist().getPlaylists(artistId))
-                        .type(new TypeReference<>() {})
-                ),
-                arguments("playlists with pagination", new StubArguments<Page<Playlist>>()
-                        .mappingBuilder(get(urlPathTemplate("/artist/{artistId}/playlists")))
-                        .pathParam("artistId", artistId)
-                        .queryParam("index", index)
-                        .queryParam("limit", limit)
-                        .file("/response/artist/get-playlists.json")
-                        .methodFactory(client -> client.artist().getPlaylists(artistId).index(index).limit(limit))
-                        .type(new TypeReference<>() {})
-                ),
-                arguments("radio", new StubArguments<Page<Track>>()
-                        .mappingBuilder(get(urlPathTemplate("/artist/{artistId}/radio")))
-                        .pathParam("artistId", artistId)
-                        .file("/response/artist/get-radio.json")
-                        .methodFactory(client -> client.artist().getRadio(artistId))
-                        .type(new TypeReference<>() {})
-                ),
-                arguments("radio with pagination", new StubArguments<Page<Track>>()
-                        .mappingBuilder(get(urlPathTemplate("/artist/{artistId}/radio")))
-                        .pathParam("artistId", artistId)
-                        .queryParam("index", index)
-                        .queryParam("limit", limit)
-                        .file("/response/artist/get-radio.json")
-                        .methodFactory(client -> client.artist().getRadio(artistId).index(index).limit(limit))
-                        .type(new TypeReference<>() {})
-                ),
-                arguments("related", new StubArguments<Page<Artist>>()
-                        .mappingBuilder(get(urlPathTemplate("/artist/{artistId}/related")))
-                        .pathParam("artistId", artistId)
-                        .file("/response/artist/get-related.json")
-                        .methodFactory(client -> client.artist().getRelated(artistId))
-                        .type(new TypeReference<>() {})
-                ),
-                arguments("related with pagination", new StubArguments<Page<Artist>>()
-                        .mappingBuilder(get(urlPathTemplate("/artist/{artistId}/related")))
-                        .pathParam("artistId", artistId)
-                        .queryParam("index", index)
-                        .queryParam("limit", limit)
-                        .file("/response/artist/get-related.json")
-                        .methodFactory(client -> client.artist().getRelated(artistId).index(index).limit(limit))
-                        .type(new TypeReference<>() {})
-                ),
-                arguments("top", new StubArguments<Page<Track>>()
-                        .mappingBuilder(get(urlPathTemplate("/artist/{artistId}/top")))
-                        .pathParam("artistId", artistId)
-                        .file("/response/artist/get-top.json")
-                        .methodFactory(client -> client.artist().getTop(artistId))
-                        .type(new TypeReference<>() {})
-                ),
-                arguments("top with pagination", new StubArguments<Page<Track>>()
-                        .mappingBuilder(get(urlPathTemplate("/artist/{artistId}/top")))
-                        .pathParam("artistId", artistId)
-                        .queryParam("index", index)
-                        .queryParam("limit", limit)
-                        .file("/response/artist/get-top.json")
-                        .methodFactory(client -> client.artist().getTop(artistId).index(index).limit(limit))
-                        .type(new TypeReference<>() {})
-                )
+        stubFor(get(urlPathTemplate("/artist/{artistId}/albums"))
+                .withPathParam("artistId", equalTo(String.valueOf(artistId)))
+                .withQueryParam("index", equalTo(String.valueOf(index)))
+                .withQueryParam("limit", equalTo(String.valueOf(limit)))
+                .willReturn(okJson(body))
         );
-    };
 
-    @ParameterizedTest(name = "should return {0}", quoteTextArguments = false)
-    @FieldSource
-    void testSuccessfulScenario(String name, StubArguments<?> args) throws IOException {
-        stubRequest(args);
+        assertEquals(expected, deezerClient.artist().getAlbums(artistId).index(index).limit(limit));
+    }
+
+    @Test
+    void shouldReturnFans() throws IOException {
+        var artistId = 5587890L;
+        var body = read("/response/artist/get-fans.json");
+        var expected = MAPPER.readValue(body, new TypeReference<Page<User>>() {});
+
+        stubFor(get(urlPathTemplate("/artist/{artistId}/fans"))
+                .withPathParam("artistId", equalTo(String.valueOf(artistId)))
+                .willReturn(okJson(body))
+        );
+
+        assertEquals(expected, deezerClient.artist().getFans(artistId));
+    }
+
+    @Test
+    void shouldReturnFansWithPagination() throws IOException {
+        var artistId = 5587890L;
+        var index = 5;
+        var limit = 10;
+        var body = read("/response/artist/get-fans.json");
+        var expected = MAPPER.readValue(body, new TypeReference<Page<User>>() {});
+
+        stubFor(get(urlPathTemplate("/artist/{artistId}/fans"))
+                .withPathParam("artistId", equalTo(String.valueOf(artistId)))
+                .withQueryParam("index", equalTo(String.valueOf(index)))
+                .withQueryParam("limit", equalTo(String.valueOf(limit)))
+                .willReturn(okJson(body))
+        );
+
+        assertEquals(expected, deezerClient.artist().getFans(artistId).index(index).limit(limit));
+    }
+
+    @Test
+    void shouldReturnPlaylists() throws IOException {
+        var artistId = 5587890L;
+        var body = read("/response/artist/get-playlists.json");
+        var expected = MAPPER.readValue(body, new TypeReference<Page<Playlist>>() {});
+
+        stubFor(get(urlPathTemplate("/artist/{artistId}/playlists"))
+                .withPathParam("artistId", equalTo(String.valueOf(artistId)))
+                .willReturn(okJson(body))
+        );
+
+        assertEquals(expected, deezerClient.artist().getPlaylists(artistId));
+    }
+
+    @Test
+    void shouldReturnPlaylistsWithPagination() throws IOException {
+        var artistId = 5587890L;
+        var index = 5;
+        var limit = 10;
+        var body = read("/response/artist/get-playlists.json");
+        var expected = MAPPER.readValue(body, new TypeReference<Page<Playlist>>() {});
+
+        stubFor(get(urlPathTemplate("/artist/{artistId}/playlists"))
+                .withPathParam("artistId", equalTo(String.valueOf(artistId)))
+                .withQueryParam("index", equalTo(String.valueOf(index)))
+                .withQueryParam("limit", equalTo(String.valueOf(limit)))
+                .willReturn(okJson(body))
+        );
+
+        assertEquals(expected, deezerClient.artist().getPlaylists(artistId).index(index).limit(limit));
+    }
+
+    @Test
+    void shouldReturnRadio() throws IOException {
+        var artistId = 5587890L;
+        var body = read("/response/artist/get-radio.json");
+        var expected = MAPPER.readValue(body, new TypeReference<Page<Track>>() {});
+
+        stubFor(get(urlPathTemplate("/artist/{artistId}/radio"))
+                .withPathParam("artistId", equalTo(String.valueOf(artistId)))
+                .willReturn(okJson(body))
+        );
+
+        assertEquals(expected, deezerClient.artist().getRadio(artistId));
+    }
+
+    @Test
+    void shouldReturnRadioWithPagination() throws IOException {
+        var artistId = 5587890L;
+        var index = 5;
+        var limit = 10;
+        var body = read("/response/artist/get-radio.json");
+        var expected = MAPPER.readValue(body, new TypeReference<Page<Track>>() {});
+
+        stubFor(get(urlPathTemplate("/artist/{artistId}/radio"))
+                .withPathParam("artistId", equalTo(String.valueOf(artistId)))
+                .withQueryParam("index", equalTo(String.valueOf(index)))
+                .withQueryParam("limit", equalTo(String.valueOf(limit)))
+                .willReturn(okJson(body))
+        );
+
+        assertEquals(expected, deezerClient.artist().getRadio(artistId).index(index).limit(limit));
+    }
+
+    @Test
+    void shouldReturnRelated() throws IOException {
+        var artistId = 5587890L;
+        var body = read("/response/artist/get-related.json");
+        var expected = MAPPER.readValue(body, new TypeReference<Page<Artist>>() {});
+
+        stubFor(get(urlPathTemplate("/artist/{artistId}/related"))
+                .withPathParam("artistId", equalTo(String.valueOf(artistId)))
+                .willReturn(okJson(body))
+        );
+
+        assertEquals(expected, deezerClient.artist().getRelated(artistId));
+    }
+
+    @Test
+    void shouldReturnRelatedWithPagination() throws IOException {
+        var artistId = 5587890L;
+        var index = 5;
+        var limit = 10;
+        var body = read("/response/artist/get-related.json");
+        var expected = MAPPER.readValue(body, new TypeReference<Page<Artist>>() {});
+
+        stubFor(get(urlPathTemplate("/artist/{artistId}/related"))
+                .withPathParam("artistId", equalTo(String.valueOf(artistId)))
+                .withQueryParam("index", equalTo(String.valueOf(index)))
+                .withQueryParam("limit", equalTo(String.valueOf(limit)))
+                .willReturn(okJson(body))
+        );
+
+        assertEquals(expected, deezerClient.artist().getRelated(artistId).index(index).limit(limit));
+    }
+
+    @Test
+    void shouldReturnTop() throws IOException {
+        var artistId = 5587890L;
+        var body = read("/response/artist/get-top.json");
+        var expected = MAPPER.readValue(body, new TypeReference<Page<Track>>() {});
+
+        stubFor(get(urlPathTemplate("/artist/{artistId}/top"))
+                .withPathParam("artistId", equalTo(String.valueOf(artistId)))
+                .willReturn(okJson(body))
+        );
+
+        assertEquals(expected, deezerClient.artist().getTop(artistId));
+    }
+
+    @Test
+    void shouldReturnTopWithPagination() throws IOException {
+        var artistId = 5587890L;
+        var index = 5;
+        var limit = 10;
+        var body = read("/response/artist/get-top.json");
+        var expected = MAPPER.readValue(body, new TypeReference<Page<Track>>() {});
+
+        stubFor(get(urlPathTemplate("/artist/{artistId}/top"))
+                .withPathParam("artistId", equalTo(String.valueOf(artistId)))
+                .withQueryParam("index", equalTo(String.valueOf(index)))
+                .withQueryParam("limit", equalTo(String.valueOf(limit)))
+                .willReturn(okJson(body))
+        );
+
+        assertEquals(expected, deezerClient.artist().getTop(artistId).index(index).limit(limit));
+    }
+
+    private <T> void assertEquals(T expected, DeezerRequest<T> request) {
+        Assertions.assertEquals(expected, request.execute());
+        Assertions.assertEquals(expected, request.executeAsync().join());
+    }
+
+    private String read(String file) throws IOException {
+        @Cleanup var inputStream = Objects.requireNonNull(this.getClass().getResourceAsStream(file));
+
+        return new String(inputStream.readAllBytes());
     }
 }
